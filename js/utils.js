fe1a9909cd2bf0a0e4f47c07f4bea2e0fbde1d06
EntropyChicken <jasondeng333@gmail.com>
2026-07-16 17:28:07 -0400

    Remove redundant function

--- js/utils.js
+++ js/utils.js
@@ -9,20 +9,3 @@ var angTo = function(base, aim, strength){
         else{return(b-strength);}
     }
 };
-
-function getCanvasDims() {
-  const targetRatio = 1.5; // Width / Height
-  let w = windowWidth;
-  let h = windowHeight;
-
-  // If the window is too wide, limit width based on height
-  if (w / h > targetRatio) {
-    w = h * targetRatio;
-  } 
-  // If the window is too tall, limit height based on width
-  else {
-    h = w / targetRatio;
-  }
-
-  return { w: w, h: h };
-}
