4dfa964226a24e2b6e0dd2794f200a4034b2664f
EntropyChicken <jasondeng333@gmail.com>
2026-07-16 16:05:56 -0400

    serviceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "one-stone-cache-v1";
+const CACHE_FILES = [
+    "./", 
+    "./index.html",
+    "./style.css",
+    "./sketch.js",
+    "./libraries/p5.min.js",
+    "./libraries/p5.sound.min.js",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(CACHE_FILES))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            if (event.request.mode === 'navigate') {
+                return caches.match("./index.html");
+            }
+            return fetch(event.request).catch(() => new Response(null, { status: 404 }));
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            ))
+            .then(() => self.clients.claim())
+            .then(() => self.clients.matchAll({ type: 'window' }))
+            .then((clients) => {
+                // Tell every open tab a new version just took over
+                clients.forEach((client) => client.postMessage({ type: 'NEW_VERSION_ACTIVATED' }));
+            })
+    );
+});
